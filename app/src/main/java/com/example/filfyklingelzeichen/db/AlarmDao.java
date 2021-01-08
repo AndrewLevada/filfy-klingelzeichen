cdc02aa11edb102bacbf74a79867e008349cf6a0
max <1111>
2021-01-08 14:49:47 +0300

    More methods added to AlarmDao

--- app/src/main/java/com/example/filfyklingelzeichen/db/AlarmDao.java
+++ app/src/main/java/com/example/filfyklingelzeichen/db/AlarmDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -20,6 +21,12 @@ public interface AlarmDao {
 
     @Insert
     void insertAll(Alarm... alarm);
+
+    @Insert
+    void insert(Alarm alarm);
+
+    @Update
+    void update(Alarm alarm);
 
     @Delete
     void delete(Alarm alarm);
