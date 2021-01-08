80b7e934c0496fb627a60ed1ed206d4e1c046618
AndrewLevada <zipeofficial@gmail.com>
2021-01-08 10:35:54 +0300

    inital

---
+++ app/src/main/java/com/example/filfyklingelzeichen/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.filfyklingelzeichen;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+}
