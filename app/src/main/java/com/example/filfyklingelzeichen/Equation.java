9a2f2e83f62c55035a1dc97705863994d8664f97
max <1111>
2021-01-08 13:14:35 +0300

    RingActivity added

---
+++ app/src/main/java/com/example/filfyklingelzeichen/Equation.java
@@ -0,0 +1,73 @@
+package com.example.filfyklingelzeichen;
+
+public class Equation implements Task {
+    int b, c, x1, x2;
+
+    public Equation(Difficulty difficulty) {
+        do {
+            x1 = difficulty.getX1();
+            x2 = difficulty.getX2();
+            b = x1 + x2;
+            c = x1 * x2;
+        } while (b == 0 || c == 0);
+    }
+
+    @Override
+    public String getEquation() {
+        return "x^2 "
+                + (b > 0 ? "+ " : "- ") + Math.abs(b) + "x "
+                + (c > 0 ? "+ " : "- ") + Math.abs(c) + " = 0";
+    }
+
+    @Override
+    public int[] getAnswers() {
+        if (x1 == x2)
+            return new int[] { x1 };
+        else
+            return new int[] { x1, x2 };
+    }
+
+    public interface Difficulty {
+        int getX1();
+        int getX2();
+    }
+
+    public static class Easy implements Difficulty {
+
+        @Override
+        public int getX1() {
+            return (int) (-3 + Math.random() * 9); // [-3;6)
+        }
+
+        @Override
+        public int getX2() {
+            return (int) (-3 + Math.random() * 9); // [-3;6)
+        }
+    }
+
+    public static class Medium implements Difficulty {
+
+        @Override
+        public int getX1() {
+            return (int) (-9 + Math.random() * 20); // [-9;11)
+        }
+
+        @Override
+        public int getX2() {
+            return (int) (-9 + Math.random() * 20); // [-9;11)
+        }
+    }
+
+    public static class Hard implements Difficulty {
+
+        @Override
+        public int getX1() {
+            return (int) (-12 + Math.random() * 28); // [-12;16)
+        }
+
+        @Override
+        public int getX2() {
+            return (int) (-12 + Math.random() * 28); // [-12;16)
+        }
+    }
+}
--- app/src/main/java/com/example/filfyklingelzeichen/RingActivity.java
+++ app/src/main/java/com/example/filfyklingelzeichen/RingActivity.java
@@ -1,14 +1,85 @@
 package com.example.filfyklingelzeichen;
 
+import android.annotation.SuppressLint;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class RingActivity extends AppCompatActivity {
+    final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.default_music);
+
+    Button check = findViewById(R.id.activity_ring_check);
+
+    TextView equation_tv = findViewById(R.id.activity_ring_equation);
+    TextView message_tv = findViewById(R.id.activity_ring_message);
+
+    EditText root1_et = findViewById(R.id.activity_ring_root1);
+    EditText root2_et = findViewById(R.id.activity_ring_root2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ring);
+
+        Equation eq = new Equation(new Equation.Easy());
+        int[] ans = eq.getAnswers();
+        equation_tv.setText(eq.getEquation());
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int root1 = Integer.parseInt(root1_et.getText().toString());
+                int root2 = Integer.parseInt(root2_et.getText().toString());
+
+                if ((root1 == ans[0] && root2 == ans[1]) || (root1 == ans[1] && root2 == ans[0])) {
+                    mediaPlayer.stop();
+                    finish();
+                } else {
+                    message_tv.setText("Wrong answer");
+                }
+            }
+        });
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void playAlarmSound () {
+        new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                try {
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @SuppressLint("StaticFieldLeak")
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            mediaPlayer.reset();
+                            mediaPlayer.release();
+                        }
+                    });
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mediaPlayer.start();
+                        }
+                    });
+
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                    mediaPlayer.setVolume(1.0f, 1.0f);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.prepare();
+                    return true;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        }.execute();
     }
 }
